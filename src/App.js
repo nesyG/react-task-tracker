b308419d9de7ae3f273cf4af82ea2e68f9933a8b
Ines Gvoic <igvoic.work@gmail.com>
2022-10-03 10:17:17 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
 function App() {
+  const [tasks,setTask] = useState([
+      {
+          id: 1,
+          text: "Food shopping",
+          day: "7th February",
+          reminder: false,
+      },
+      {
+          id: 2,
+          text: "Doctor Appointment",
+          day: "8th February",
+          reminder: false,
+      },
+      {
+          id: 3,
+          text: "Coffee Date",
+          day: "6th February",
+          reminder: false,
+      }
+  ])
+
+  //Delete Task
+  const deleteTask = (id) => {
+    setTask(tasks.filter((task) => task.id !== id))
+  }
+
+  //Toggle Reminder
+const toggleReminder = (id) => {
+  setTask(tasks.map((task)=> task.id === id ? {...task, reminder: !task.reminder} : task))
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <Header title="Track your tasks!"/>
+    {tasks.length > 0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No tasks"} 
     </div>
+    
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Button from './Button'
+
+const Header = ({title}) => {
+    const onClick = () => {
+        console.log('click')
+    }
+
+  return (
+    <header className="header-style">
+        <h1>{title}</h1>
+        <Button color="steelblue" text="Add" onClick={onClick}/>
+    </header>
+  )
+}
+
+export default Header
